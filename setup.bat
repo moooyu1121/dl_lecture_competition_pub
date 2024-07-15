f22d1b54eff80e1cefb083de8c53196ac5e83dfb
moooyu1121 <87175394+moooyu1121@users.noreply.github.com>
2024-07-16 03:27:34 +0900

    environment...

--- setup.bat
+++ setup.bat
@@ -1,6 +1,7 @@
-python -m venv .venv
+py -3.10 -m venv .venv
 call .venv\Scripts\activate
 .venv\Scripts\python.exe -m pip install --upgrade pip
 pip install -r requirements.txt
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 
 pause
